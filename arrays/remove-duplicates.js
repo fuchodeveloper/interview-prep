4038bd6336f25a74d5bf58743c22795d3f2adaa4
fuchodeveloper <fmgbeoma@yahoo.com>
2021-07-06 21:54:50 +0200

    array: remove duplicates

---
+++ arrays/remove-duplicates.js
@@ -0,0 +1,17 @@
+function removeDuplicates(nums) {
+  let n = nums.length;
+
+  if (n === 0) return 0;
+
+  /* Two pointer technique 
+  i => slow runner
+  j => fast runner
+  */
+  let i = 0;
+  for(let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      nums[++i] = nums[j]
+    }
+  }
+  return i + 1;
+}
